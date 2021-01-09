f5d16e5fa5c093b5eb74f5592a9568e61f1d4bdc
zhujiejun <2383322468@qq.com>
2021-01-09 15:04:33 +0800

    commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -n 'input the act(clean build idea DistTar):'; read act;
+
+if [[ -z $act ]]; then
+	./gradlew -q clean;
+else
+	#./gradlew -q -x test $act;
+	./gradlew -q $act;
+fi
