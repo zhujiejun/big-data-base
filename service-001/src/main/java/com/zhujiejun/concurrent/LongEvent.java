bbe147f9bc1822c9b6d44eeccb43645701af2c1f
zhujiejun <2383322468@qq.com>
2021-02-02 17:08:46 +0800

    commit

---
+++ service-001/src/main/java/com/zhujiejun/concurrent/LongEvent.java
@@ -0,0 +1,9 @@
+package com.zhujiejun.concurrent;
+
+public class LongEvent {
+    private long value;
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+}
---
+++ service-001/src/main/java/com/zhujiejun/concurrent/LongEventFactory.java
@@ -0,0 +1,7 @@
+package com.zhujiejun.concurrent;
+
+public class LongEventFactory {
+    public LongEvent newInstance() {
+        return new LongEvent();
+    }
+}
