73197c76c0707fbd6eff270e2a547af899378620
zhujiejun <2383322468@qq.com>
2021-02-07 12:02:16 +0800

    commit

--- service-001/src/main/java/com/zhujiejun/concurrent/StringEventMain.java
+++ service-001/src/main/java/com/zhujiejun/concurrent/StringEventMain.java
@@ -22,17 +22,19 @@ public class StringEventMain {
         Disruptor<StringEvent> disruptor = new Disruptor<>(StringEvent::new, bufferSize, DaemonThreadFactory.INSTANCE);
 
         disruptor.handleEventsWith((e, s, b) -> log.info("----------init string is: {}----------", e.getValue()))
-                .then(handle("_A"), handle("_B"), handle("_C"))
-                .then(handle("_D"), handle("_E"), handle("_F"))
-                .then((e, s, b) -> log.info("----------final string is: {}----------", e.getValue()));
+                .then(handle("|"))
+                .then(handle("-A"), handle("-B"), handle("-C"))
+                .then(handle("|"))
+                .then(handle("-D"), handle("-E"), handle("-F"))
+                .then((e, s, b) -> log.info("----------processed string is: {}----------", e.getValue()));
         RingBuffer<StringEvent> ringBuffer = disruptor.getRingBuffer();
 
         disruptor.start();
         IntStream.rangeClosed(1, 1).forEach(i -> {
-            String initString = RandomStringUtils.randomAlphanumeric(10);
+            String initString = RandomStringUtils.randomAlphabetic(10);
             ringBuffer.publishEvent((event, sequence, buffer) -> event.setValue(initString));
             try {
-                TimeUnit.MILLISECONDS.sleep(10000);
+                TimeUnit.MILLISECONDS.sleep(250);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
