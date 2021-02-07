2b29aa27ecca3cfb5459efae41821fc7aebfe4bf
zhujiejun <2383322468@qq.com>
2021-02-07 12:59:14 +0800

    commit

--- service-001/src/main/java/com/zhujiejun/concurrent/StringEventMain.java
+++ service-001/src/main/java/com/zhujiejun/concurrent/StringEventMain.java
@@ -1,13 +1,12 @@
 package com.zhujiejun.concurrent;
 
-import com.lmax.disruptor.BlockingWaitStrategy;
 import com.lmax.disruptor.EventHandler;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
-import com.lmax.disruptor.dsl.ProducerType;
 import com.lmax.disruptor.util.DaemonThreadFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
@@ -15,7 +14,16 @@ import java.util.stream.IntStream;
 @Slf4j
 public class StringEventMain {
 
-    private static EventHandler<StringEvent> handle(String suffix) {
+    private static EventHandler<StringEvent> handleDelimiter(String delimiter) {
+        return (event, sequence, endOfBatch) -> event.setValue(event.getValue().concat(delimiter));
+    }
+
+    private static EventHandler<StringEvent> handleGroup(String suffix) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(RandomUtils.nextInt(100, 1 << 10));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return (event, sequence, endOfBatch) -> event.setValue(event.getValue().concat(suffix));
     }
 
@@ -28,19 +36,19 @@ public class StringEventMain {
         Disruptor<StringEvent> disruptor = new Disruptor<>(StringEvent::new, bufferSize, DaemonThreadFactory.INSTANCE);
 
         disruptor.handleEventsWith(show("init"))
-                .then(handle("|"))
-                .then(handle("-A"), handle("-B"), handle("-C"))
-                .then(handle("|"))
-                .then(handle("-D"), handle("-E"), handle("-F"))
+                .then(handleDelimiter("|"))
+                .then(handleGroup("-A"), handleGroup("-B"), handleGroup("-C"))
+                .then(handleDelimiter("|"))
+                .then(handleGroup("-D"), handleGroup("-E"), handleGroup("-F"))
                 .then(show("processed"));
         RingBuffer<StringEvent> ringBuffer = disruptor.getRingBuffer();
 
         disruptor.start();
-        IntStream.rangeClosed(1, 1).forEach(i -> {
+        IntStream.rangeClosed(1, 3).forEach(i -> {
             String initString = RandomStringUtils.randomAlphabetic(10);
             ringBuffer.publishEvent((event, sequence, buffer) -> event.setValue(initString));
             try {
-                TimeUnit.MILLISECONDS.sleep(250);
+                TimeUnit.MILLISECONDS.sleep(RandomUtils.nextInt(10, 1 << 6));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
