accb0f8a1272072154826ad976272d0d09e90c36
zhujiejun <2383322468@qq.com>
2021-03-09 11:40:29 +0800

    commit

--- service-001/src/main/java/com/zhujiejun/concurrent/StringEventMain.java
+++ service-001/src/main/java/com/zhujiejun/concurrent/StringEventMain.java
@@ -1,5 +1,6 @@
 package com.zhujiejun.concurrent;
 
+import com.google.common.base.Stopwatch;
 import com.lmax.disruptor.EventHandler;
 import com.lmax.disruptor.YieldingWaitStrategy;
 import com.lmax.disruptor.dsl.Disruptor;
@@ -7,7 +8,6 @@ import com.lmax.disruptor.dsl.ProducerType;
 import com.lmax.disruptor.util.DaemonThreadFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.concurrent.Executor;
@@ -17,65 +17,63 @@ import java.util.stream.IntStream;
 
 @Slf4j
 public class StringEventMain {
-
-    private static final Executor executor = Executors.newFixedThreadPool(3);
+    private static final Executor executor = Executors.newCachedThreadPool();
 
     private static EventHandler<StringEvent> show(String msg) {
-        log.info("==========1.current thread is: {}==========", Thread.currentThread().toString());
-        return (event, sequence, endOfBatch) ->
-                log.info("=========={} string is: [{}]============{}",
-                        msg, event.getValue(),
-                        StringUtils.equals(msg, "processed") ? "\n" : "");
+        return (event, sequence, endOfBatch) -> {
+            log.info("==========1.current thread is: {}==========", Thread.currentThread().toString());
+            log.info("=========={} string is: [{}]============{}", msg, event.getValue(), StringUtils.equals(msg, "processed") ? "\n" : "");
+        };
     }
 
     private static EventHandler<StringEvent> delimit(String delimiter) {
-        log.info("==========2.current thread is: {}==========", Thread.currentThread().toString());
-        return (event, sequence, endOfBatch) -> event.setValue(event.getValue().concat(delimiter));
+        return (event, sequence, endOfBatch) -> {
+            log.info("==========2.current thread is: {}==========", Thread.currentThread().toString());
+            event.setValue(event.getValue().concat(delimiter));
+        };
     }
 
-    private static EventHandler<StringEvent> handle(String suffix) {
-
+    private static EventHandler<StringEvent> append(String suffix) {
         return (event, sequence, endOfBatch) -> {
             log.info("==========3.current thread is: {}==========", Thread.currentThread().toString());
-            try {
-                TimeUnit.MILLISECONDS.sleep(RandomUtils.nextInt(100, 500));
+            /*try {
+                TimeUnit.MILLISECONDS.sleep(RandomUtils.nextInt(1 << 7, 1 << 10));
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
+            }*/
             event.setValue(event.getValue().concat(suffix));
         };
     }
 
     public static void main(String[] args) {
-        int bufferSize = 1 << 12;
+        int bufferSize = 1 << 20;
+        Stopwatch watch = Stopwatch.createStarted();
+
         Disruptor<StringEvent> disruptor = new Disruptor<>(StringEvent::new, bufferSize,
                 DaemonThreadFactory.INSTANCE, ProducerType.MULTI, new YieldingWaitStrategy());
 
         disruptor.handleEventsWith(show("initiated"))
                 .then(delimit("|"))
-                .then(handle("-A"), handle("-B"))
+                .then(append("-A"), append("-B"))
                 .then(delimit("|"))
-                .then(handle("-C"), handle("-D"))
+                .then(append("-C"), append("-D"))
                 .then(delimit("|"))
-                .then(handle("-E"), handle("-F"))
+                .then(append("-E"), append("-F"))
                 .then(show("processed"));
 
         disruptor.start();
-        //RingBuffer<StringEvent> ringBuffer = disruptor.getRingBuffer();
-        IntStream.rangeClosed(1, 2).forEach(i -> {
-            String initString = RandomStringUtils.randomAlphabetic(1 << 4);
-            //ringBuffer.publishEvent((event, sequence, buffer) -> event.setValue(initString));
+
+        IntStream.rangeClosed(1, 1).forEach(i -> {
+            String initString = RandomStringUtils.randomAlphabetic(1 << 5);
             disruptor.publishEvent((event, sequence) -> event.setValue(initString));
-            try {
+            /*try {
                 TimeUnit.SECONDS.sleep(RandomUtils.nextInt(1, 5));
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
+            }*/
         });
-        try {
-            TimeUnit.MILLISECONDS.sleep(Integer.MAX_VALUE);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        disruptor.shutdown();
+        log.warn("==========total time consumption is: {} ms.==========", watch.elapsed(TimeUnit.MILLISECONDS));
     }
 }
