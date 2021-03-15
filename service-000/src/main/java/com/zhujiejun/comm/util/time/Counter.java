e267aef9873c87fc7a8ad1c9a5aa3644831b4bfb
zhujiejun <2383322468@qq.com>
2021-03-15 10:30:06 +0800

    commit

---
+++ service-000/src/main/java/com/zhujiejun/comm/util/time/Counter.java
@@ -0,0 +1,13 @@
+package com.zhujiejun.comm.util.time;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Counter {
+
+}
