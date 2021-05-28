b44e6a4b67e965e270979e8296af9e684e42b245
zhujiejun <2383322468@qq.com>
2021-05-28 18:05:21 +0800

    commit

--- service-001/src/main/scala/com/zhujiejun/algorithm/Module001.scala
+++ service-001/src/main/scala/com/zhujiejun/algorithm/Module001.scala
@@ -13,5 +13,29 @@ object Module001 {
         }
         throw new IllegalArgumentException("No two sum solution")
     }
+
+    def addTwoNumbers001(l1: Nothing, l2: Nothing): Nothing = {
+        val dummyHead = new Nothing(0)
+        var p = l1
+        var q = l2
+        var curr = dummyHead
+        var carry = 0
+        while ( {
+            p != null || q != null
+        }) {
+            val x = if (p != null) p.`val`
+            else 0
+            val y = if (q != null) q.`val`
+            else 0
+            val sum = carry + x + y
+            carry = sum / 10
+            curr.next = new Nothing(sum % 10)
+            curr = curr.next
+            if (p != null) p = p.next
+            if (q != null) q = q.next
+        }
+        if (carry > 0) curr.next = new Nothing(carry)
+        dummyHead.next
+    }
 }
 
